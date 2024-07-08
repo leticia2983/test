6cefe82d3c6c240b792d70fc210020e66076d3f6
NevilleBAC <neville_bac@outlook.com>
2024-07-08 13:45:55 +0530

    added files

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,10 +1,10 @@
-output "ecs_cluster_id" {
-  value = aws_ecs_cluster.main.id
-}
-
-output "ecs_service_name" {
-  value = aws_ecs_service.strapi.name
-}
+#output "ecs_cluster_id" {
+#  value = aws_ecs_cluster.main.id
+#}
+#
+#output "ecs_service_name" {
+#  value = aws_ecs_service.strapi.name
+#}
 
 output "vpc_id" {
   value = aws_vpc.main.id
