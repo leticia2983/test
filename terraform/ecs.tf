dacf76a8edbd5e033102112c3c46fec07d35aa90
NevilleBAC <neville_bac@outlook.com>
2024-07-08 13:44:39 +0530

    added files

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -1,50 +1,50 @@
-resource "aws_ecs_cluster" "main" {
-  name = var.cluster_name
-}
-
-resource "aws_ecs_task_definition" "strapi" {
-  family                   = "strapi-let5"
-  network_mode             = "awsvpc"
-  requires_compatibilities = ["FARGATE"]
-  cpu                      = "1024"
-  memory                   = "2048"
-
-  container_definitions = jsonencode([
-    {
-      name      = "strapi"
-      image     = "leticia888444/strapi:1.0"
-      essential = true
-      portMappings = [
-        {
-          containerPort = 1337
-          hostPort      = 1337
-        }
-      ]
-    }
-  ])
-}
-resource "aws_ecs_service" "strapi" {
-  name            = "strapi-service"
-  cluster         = aws_ecs_cluster.main.id
-  task_definition = aws_ecs_task_definition.strapi.arn
-  desired_count   = 1
-
-  capacity_provider_strategy {
-    capacity_provider = "FARGATE_SPOT"
-    weight            = 1
-  }
-
-  network_configuration {
-    subnets         = var.subnet_ids
-    assign_public_ip = true
-    security_groups = [aws_security_group.strapi_sg.id]
-  }
-
-  load_balancer {
-    target_group_arn = aws_lb_target_group.strapi_tg.arn
-    container_name   = "strapi"
-    container_port   = 1337
-  }
-
-  depends_on = [aws_lb_listener.http_listener]
-}
+#resource "aws_ecs_cluster" "main" {
+#  name = var.cluster_name
+#}
+#
+#resource "aws_ecs_task_definition" "strapi" {
+#  family                   = "strapi-let5"
+#  network_mode             = "awsvpc"
+#  requires_compatibilities = ["FARGATE"]
+#  cpu                      = "1024"
+#  memory                   = "2048"
+#
+#  container_definitions = jsonencode([
+#    {
+#      name      = "strapi"
+#      image     = "leticia888444/strapi:1.0"
+#      essential = true
+#      portMappings = [
+#        {
+#          containerPort = 1337
+#          hostPort      = 1337
+#        }
+#      ]
+#    }
+#  ])
+#}
+#resource "aws_ecs_service" "strapi" {
+#  name            = "strapi-service"
+#  cluster         = aws_ecs_cluster.main.id
+#  task_definition = aws_ecs_task_definition.strapi.arn
+#  desired_count   = 1
+#
+#  capacity_provider_strategy {
+#    capacity_provider = "FARGATE_SPOT"
+#    weight            = 1
+#  }
+#
+#  network_configuration {
+#    subnets         = var.subnet_ids
+#    assign_public_ip = true
+#    security_groups = [aws_security_group.strapi_sg.id]
+#  }
+#
+#  load_balancer {
+#    target_group_arn = aws_lb_target_group.strapi_tg.arn
+#    container_name   = "strapi"
+#    container_port   = 1337
+#  }
+#
+#  depends_on = [aws_lb_listener.http_listener]
+#}
