8aabeb81eeb8dcc51d6f5480ab2494d051c22a7c
NevilleBAC <neville_bac@outlook.com>
2024-07-08 13:35:39 +0530

    added files

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,7 +19,7 @@ resource "aws_internet_gateway" "main" {
 resource "aws_subnet" "public_1" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-east-2a"
+  availability_zone       = "us-west-2b"
   map_public_ip_on_launch = true
 
   tags = {
@@ -31,7 +31,7 @@ resource "aws_subnet" "public_1" {
 resource "aws_subnet" "public_2" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.2.0/24"
-  availability_zone       = "us-east-2b"
+  availability_zone       = "us-west-2c"
   map_public_ip_on_launch = true
 
   tags = {
--- terraform/terraform.tfvars
+++ terraform/terraform.tfvars
@@ -1,4 +1,4 @@
-vpc_id         = "vpc-06f84855e455f0f74"
-subnet_ids     = ["subnet-0e859ba0f4f25c96c", "subnet-001e8795904e783e6"]
+vpc_id         = ""
+subnet_ids     = ["", ""]
 route53_zone_id = "Z06607023RJWXGXD2ZL6M"
 sub_domain      = "leticia.contentecho.in"
