881433e79af05f84366b4a868247a895f53ea928
NevilleBAC <neville_bac@outlook.com>
2024-07-08 14:07:07 +0530

    added files

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -6,10 +6,10 @@ output "ecs_service_name" {
   value = aws_ecs_service.strapi.name
 }
 
-#output "vpc_id" {
-#  value = aws_vpc.main.id
-#}
-#
-#output "public_subnet_ids" {
-#  value = [aws_subnet.public_1.id, aws_subnet.public_2.id]
-#}
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "public_subnet_ids" {
+  value = [aws_subnet.public_1.id, aws_subnet.public_2.id]
+}
--- terraform/terraform.tfvars
+++ terraform/terraform.tfvars
@@ -1,4 +1,4 @@
-vpc_id         = ""
-subnet_ids     = ["", ""]
+vpc_id         = "vpc-0a09c9badfd91ffe7"
+subnet_ids     = ["subnet-051e130cf47d29b82", "subnet-09acd8d3520d611c6"]
 route53_zone_id = "Z06607023RJWXGXD2ZL6M"
 sub_domain      = "leticia.contentecho.in"
